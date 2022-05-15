a119872d52d4f576cf1910107ca6c57de495e7e7
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2022-05-15 20:57:10 +0530

    Changes to Site

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let list = document.getElementsByClassName('image')
+/*
+let open = document.getElementById('open')
+let close = document.getElementById('closed')
+*/
+let labstatus = document.getElementById('LabStatus')
+console.log(list)
+let vidList = document.getElementsByClassName('video')
+let carousal = document.getElementById('carousal')
+console.log(carousal.clientHeight)
+console.log(carousal.clientWidth)
+/*
+function windowWasResized() {
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        element.style.height = carousal.clientHeight + 'px'
+        element.style.width = carousal.clientWidth + 'px'
+    }
+}
+*/
+data = {
+    "status": "CLOSED"
+}
+setStatus(data.status)
+
+window.addEventListener('resize', resizeImages)
+
+window.onload = function () {
+    resizeImages()
+}
+
+setInterval(function () {
+    setStatus(data.status)
+}, 5000);
+
+function setStatus(status) {
+    if (status == "OPEN") {
+        labstatus.innerHTML = "OPEN"
+        labstatus.style.backgroundColor = "green"
+        labstatus.style.fontSize = "5rem"
+    }
+    else {
+        labstatus.innerHTML = "CLOSED"
+        labstatus.style.backgroundColor = "red"
+        labstatus.style.fontSize = "4rem"
+    }
+}
+
+function resizeImages() {
+
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index].getElementsByTagName('img')[0];
+        console.log(element.naturalHeight)
+        console.log(element.naturalWidth)
+        console.log(element.naturalHeight > element.naturalWidth)
+        if (element.naturalHeight > element.naturalWidth) {
+            x = element.naturalWidth
+            element.width = carousal.clientWidth
+            console.log("x")
+            element.height = carousal.clientWidth * element.naturalHeight / x
+        }
+        else if (element.naturalHeight < element.naturalWidth) {
+            console.log("y")
+            y = element.naturalHeight
+            element.height = carousal.clientHeight
+            element.width = carousal.clientHeight * element.naturalWidth / y
+        }
+
+    }
+}
