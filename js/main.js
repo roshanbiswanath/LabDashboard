ddc0ba8b93e8d0b6150e5d45246e9fb6df97333f
P.Biswanath Patra <63574390+roshanbiswanath@users.noreply.github.com>
2022-05-21 18:14:02 +0530

    Changes to Site

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-let list = document.getElementsByClassName('image')
 /*
 let open = document.getElementById('open')
 let close = document.getElementById('closed')
 */
 let labstatus = document.getElementById('LabStatus')
-console.log(list)
-let vidList = document.getElementsByClassName('video')
-let carousal = document.getElementById('carousal')
-console.log(carousal.clientHeight)
-console.log(carousal.clientWidth)
+
 /*
 function windowWasResized() {
     for (let index = 0; index < list.length; index++) {
@@ -19,8 +14,42 @@ function windowWasResized() {
 }
 */
 data = {
-    "status": "OPEN"
+    "status": "OPEN",
+    "nextEventTime": "June 5, 2022 15:37:25",
+    "carouselImages": [
+        "./assets/271191258_2057244351099308_9022260579650513705_n.jpg",
+        "./assets/DSC_5709.jpg"
+    ],
+    "carouselVideos": [
+        "./assets/lab.mp4"
+    ]
 }
+
+const imageTemp1 = '<div class="carousel-item"><div class="image"><img src="';
+const imageTemp2 = '" alt="..."></div></div>'
+const videoTemp1 = '<div class="carousel-item" data-bs-interval="8000"><video class="video" loop><source src="'
+const videoTemp2 = '" type="video/mp4"></video></div>'
+carousalInner = ""
+
+
+for (let index = 0; index < data["carouselImages"].length; index++) {
+    const element = data["carouselImages"][index];
+    carousalInner += imageTemp1 + element + imageTemp2;
+}
+for (let index = 0; index < data["carouselVideos"].length; index++) {
+    const element = data["carouselVideos"][index];
+    carousalInner += videoTemp1 + element + videoTemp2;
+
+}
+let carousal = document.getElementById('carousal')
+let carousalinner = document.getElementsByClassName('carousel-inner')[0];
+carousalinner.innerHTML = carousalInner;
+document.getElementsByClassName('carousel-item')[0].classList.add('active')
+let list = document.getElementsByClassName('image')
+
+let vidList = document.getElementsByClassName('video')
+console.log(carousal.clientHeight)
+console.log(carousal.clientWidth)
 setStatus(data.status)
 
 window.addEventListener('resize', resizeImages)
@@ -69,37 +98,48 @@ function resizeImages() {
     }
 }
 
+var countdown = document.getElementsByClassName('countdown')[0]
 
 var dayEle = document.getElementById("dayEle");
 var hourEle = document.getElementById("hourEle");
 var minuteEle = document.getElementById("minuteEle");
 var secondEle = document.getElementById("secondEle");
 // Set the date we're counting down to
-var countDownDate = new Date("July 5, 2022 15:37:25").getTime();
+var countDownDate = new Date(data["nextEventTime"]).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
-
     // Get today's date and time
-    var now = new Date().getTime();
-
+    var now = new Date();
+    /*
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
 
     // Time calculations for days, hours, minutes and seconds
+    var distance = now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    dayEle.innerHTML = days;
-    hourEle.innerHTML = hours;
-    minuteEle.innerHTML = minutes;
+    dayEle.innerHTML = now.prototype.getHours();
+    hourEle.innerHTML = minutes;
+    minuteEle.innerHTML = seconds;
     secondEle.innerHTML = seconds;
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
-    }
+    */
+    hour = now.getHours();
+    ampm = hour >= 12 ? 'PM' : 'AM';
+    hour = hour % 12;
+    hour = hour ? hour : 12; // the hour '0' should be '12'
+    minutes = now.getMinutes();
+    seconds = now.getSeconds();
+    //dayEle.innerHTML = hour;
+    //hourEle.innerHTML = minutes;
+    //minuteEle.innerHTML = seconds;
+    //secondEle.innerHTML = ampm;
+    countdown.innerHTML = hour + ":" + minutes + ":" + seconds + " " + ampm;
+
+
 }, 1000);
+
